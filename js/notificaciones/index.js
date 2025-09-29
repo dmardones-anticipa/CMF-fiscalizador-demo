51b4aef36302398f6810fbeddc5af2e268022144
csandoval-anticipa <csandovalh@anticipa.cl>
2025-09-29 17:18:43 -0300

    Se ocultan titulos y modifica data condicional por frecuencia

--- js/notificaciones/index.js
+++ js/notificaciones/index.js
@@ -4,8 +4,10 @@ import { cargarSistemas } from './sistemas/cargarSistemas.js';
 (() => {
     console.log("INICIANDO NOTIFICACIONES")
 
+    const tituloResultado = document.getElementsByClassName("title");
     const resultado = document.getElementsByClassName("resultados");
     const botonBuscar = document.getElementsByClassName("buttonPrimary")
+    const sistemasContainer = document.getElementById("Sistemas-section")
     
 
     console.log(resultado)
@@ -13,21 +15,25 @@ import { cargarSistemas } from './sistemas/cargarSistemas.js';
    
 
     resultado[0].style.display = "none"
+    sistemasContainer.style.display = "none"
+    tituloResultado[1].style.display = "none"
 
     botonBuscar[0].addEventListener("click", () => {
         resultado[0].style.display = "grid"
+        tituloResultado[1].style.display = "block"
 
         const cardResultados = document.querySelectorAll(".frecuencia")
 
-         console.log(cardResultados)
+        console.log(cardResultados)
 
-         cardResultados.forEach((item) => {
-            console.log(item)
-            item.addEventListener("click", () => {
-                console.log("click", item.dataset.frecuencia)
-                cargarSistemas(item.dataset.frecuencia)
-            })
-         })
+        cardResultados.forEach((item) => {
+        console.log(item)
+        item.addEventListener("click", () => {
+            console.log("click", item.dataset.frecuencia)
+            sistemasContainer.style.display = "block"
+            cargarSistemas(item.dataset.frecuencia)
+        })
+        })
     })
 
 })()
